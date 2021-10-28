ae8751be4ceba84158ec184bb5d75ca3735daab5
johnconnor <zkmd2012@inbox.ru>
2021-10-28 17:43:33 +0500

    6 useFetch to fetch data of a single item, useParams to extract id parameter from the URL

--- src/pages/recipe/Recipe.jsx
+++ src/pages/recipe/Recipe.jsx
@@ -1,9 +1,20 @@
 import './Recipe.css'
+import { useEffect } from 'react'
+import { useFetch } from '../../hooks/useFetch'
+import { useParams } from 'react-router-dom'
 
 const Recipe = () => {
+
+    const {resId} = useParams()
+
+    const {data: recipe, isPending, error} = useFetch(`http://localhost:3000/recipes/${resId}`)
+
+
     return (
         <div>
-            Recipe
+            {error && <p className="error">{error}</p>}
+            {isPending && <p className="loading">Loading...</p>}
+            {recipe && <h1>{recipe.title}</h1>}
         </div>
     )
 }
