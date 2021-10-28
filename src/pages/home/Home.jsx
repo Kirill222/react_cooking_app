f59df23143e6993dfbfe407900b7281b15609e91
johnconnor <zkmd2012@inbox.ru>
2021-10-28 16:53:43 +0500

    4 Reuse of custom hook useFetch, fetching and displaying data

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,9 +1,17 @@
 import './Home.css'
+import {useFetch} from '../../hooks/useFetch'
 
 const Home = () => {
+
+    const {data: recepies, isPending, error} = useFetch('http://localhost:3000/recipes')
+
     return (
-        <div>
-            Home
+        <div className="home">
+            {error && <p className="error">{error}</p>}
+            {isPending && <p className="loading">Loading...</p>}
+            {recepies && recepies.map(recipe => (                
+                <h2 key={recipe.id}>{recipe.title}</h2>                                
+            ))}
         </div>
     )
 }
