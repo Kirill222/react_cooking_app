584fb2a5dab50dfaccfad2d92fd3b4e13868a53a
johnconnor <zkmd2012@inbox.ru>
2021-10-29 13:59:43 +0500

    12 Conditional rendering of the templates of a component

--- src/components/RecipeList.jsx
+++ src/components/RecipeList.jsx
@@ -2,6 +2,12 @@ import './RecipeList.css'
 import {Link} from "react-router-dom"
 
 const RecipeList = ({recipes}) => {
+
+
+    if (recipes.length === 0) {
+        return <div className="error">No data found</div>
+    }
+
     return (
         <div className="recipe-list">
             {recipes.map(recipe => (
