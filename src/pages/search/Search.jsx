518f8433213b56c186b871653d40842db49c3fcb
johnconnor <zkmd2012@inbox.ru>
2021-10-29 12:34:42 +0500

    11.1 Search logic: fetch request with query parameters, q stands for 'query in URL', useLocation and URLSearchParams for extracting query params from URL

--- src/pages/search/Search.jsx
+++ src/pages/search/Search.jsx
@@ -1,9 +1,24 @@
+import { useLocation } from 'react-router-dom'
+import { useFetch } from '../../hooks/useFetch'
+import RecipeList from '../../components/RecipeList'
 import './Search.css'
 
 const Search = () => {
+
+    const queryString = useLocation().search
+    const queryParams = new URLSearchParams(queryString)
+    const term = queryParams.get('term')
+
+    const {data: searchResults, isPending, error } = useFetch(`http://localhost:3000/recipes?q=${term}`)
+
+
     return (
         <div>
-            Search
+            <h2 className="page-title">Recipes icluding "{term}"</h2>
+            {error && <p className="error">{error}</p>}
+            {isPending && <p className="loading">Loading...</p>}
+            {searchResults && <RecipeList recipes={searchResults} /> }
+
         </div>
     )
 }
