14837a90682406d449d8d28d8297954db5df5bce
johnconnor <zkmd2012@inbox.ru>
2021-10-29 16:36:57 +0500

    16 Reducers and useReducer. The logic upon which Redux is built. Using useReducer to change theme color

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,11 +5,11 @@ import {useTheme} from '../hooks/useTheme'
 
 const Navbar = () => {
 
-    const {color} = useTheme()
+    const {color, changeColor} = useTheme()
 
     return (
         <div className="navbar" style={{background: color}}>
-            <nav>
+            <nav onClick={() => changeColor('green')}>
                 <Link exact="true" to="/" className="brand">
                     <h1>Cooking app</h1>
                 </Link>
