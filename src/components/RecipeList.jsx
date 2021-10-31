507f5b75e0787bf3bf487f66f8b95d0c79a3f566
John Connor <zkmd2012@inbox.ru>
2021-10-31 13:09:37 +0500

    23 DELETE data from firestore. (In order to update the UI we need to use real time data which will be in following commits)

--- src/components/RecipeList.jsx
+++ src/components/RecipeList.jsx
@@ -1,16 +1,23 @@
 import './RecipeList.css'
 import {Link} from "react-router-dom"
 import { useTheme } from '../hooks/useTheme'
+import DeleteIcon from '../assets/delete.svg'
+import {projectFirestore} from '../firebase/config'
+
 
 
 const RecipeList = ({recipes}) => {
 
     const {mode} = useTheme()
 
-
-
     if (recipes.length === 0) {
         return <div className="error">No data found</div>
+    }
+
+    const handleDelete = async (id) => {
+
+        await projectFirestore.collection('recipes').doc(id).delete()
+
     }
 
     return (
@@ -21,6 +28,11 @@ const RecipeList = ({recipes}) => {
                     <p>{recipe.coolingTime} to make.</p>
                     <div>{recipe.method.substring(0, 100)}...</div>
                     <Link to={`/recipes/${recipe.id}`}>Cook this</Link>
+                    <img 
+                        src={DeleteIcon}
+                        onClick={() => handleDelete(recipe.id)}
+                        className="delete"
+                    />
                 </div>
             ))}
         </div>
